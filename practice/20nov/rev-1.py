f92ac54bae843701b0e47add3ea56588526816a6
nandansn <geeknanda13@gmail.com>
2022-11-20 16:30:59 +0530

    updated

---
+++ practice/20nov/rev-1.py
@@ -0,0 +1,46 @@
+import  math
+
+name = "nanda\nsanmathi\nnivrithi"
+
+print(name)
+
+# command line input
+
+# userName = input("name:")
+# age = input("age:")
+# height = input("height in ft:")
+# weight = input("weight in kg:")
+
+
+# bmi = round(float(weight) / (float(height) * 0.3048) ** 2,2)
+
+# print(f"Hello, {userName} your BMI is {bmi}")
+
+
+# if (bmi > 30) :
+#     print('over-weight')
+# elif (bmi > 25 and bmi < 30):
+#     print('little over weight')
+# else:
+#     print('weight ok')
+
+# max_age_limit = 90
+# year_left = (max_age_limit - int(age))
+# days_left =  year_left * 365
+
+# months_left = days_left / 12
+
+# weeks_left = months_left * 4
+
+
+
+
+# print(f'{userName} you got {days_left} days, {math.ceil(months_left)} months and {math.ceil(weeks_left)}')
+
+stocks = [{"name":"Hindustan Unilever Ltd","CMP":"2,483","PE":"60.48"},{"name":"Asian Paints Ltd","CMP":"2,483","PE":"60.48"},{"name":"HCL Technologies Ltd","CMP":"2,483","PE":"60.48"},{"name":"State Bank Of India","CMP":"2,483","PE":"60.48"},{"name":"Kotak Mahindra Bank Ltd","CMP":"2,483","PE":"60.48"},{"name":"Infosys Ltd","CMP":"2,483","PE":"60.48"},{"name":"Housing Development Finance Corporation Ltd","CMP":"2,483","PE":"60.48"},{"name":"Tech Mahindra Ltd","CMP":"2,483","PE":"60.48"},{"name":"Axis Bank Ltd","CMP":"2,483","PE":"60.48"},{"name":"ICICI Bank Ltd","CMP":"2,483","PE":"60.48"},{"name":"HDFC Life Insurance Co Ltd","CMP":"2,483","PE":"60.48"},{"name":"Divis Laboratories Ltd","CMP":"2,483","PE":"60.48"},{"name":"Adani Enterprises Ltd","CMP":"2,483","PE":"60.48"},{"name":"Dr. Reddys Laboratories Ltd","CMP":"2,483","PE":"60.48"},{"name":"JSW Steel Ltd","CMP":"2,483","PE":"60.48"},{"name":"Tata Motors Ltd","CMP":"2,483","PE":"60.48"},{"name":"Apollo Hospitals Enterprise Ltd","CMP":"2,483","PE":"60.48"},{"name":"Power Grid Corporation Of India Ltd","CMP":"2,483","PE":"60.48"},{"name":"Reliance Industries Ltd","CMP":"2,483","PE":"60.48"},{"name":"Larsen & Toubro Ltd","CMP":"2,483","PE":"60.48"},{"name":"Adani Ports and Special Economic Zone Ltd","CMP":"2,483","PE":"60.48"},{"name":"Nestle India Ltd","CMP":"2,483","PE":"60.48"},{"name":"HDFC Bank Ltd","CMP":"2,483","PE":"60.48"},{"name":"Grasim Industries Ltd","CMP":"2,483","PE":"60.48"},{"name":"Tata Consultancy Services Ltd","CMP":"2,483","PE":"60.48"},{"name":"Tata Steel Ltd","CMP":"2,483","PE":"60.48"},{"name":"Bharat Petroleum Corporation Ltd","CMP":"2,483","PE":"60.48"},{"name":"Hindalco Industries Ltd","CMP":"2,483","PE":"60.48"},{"name":"Sun Pharmaceutical Industries Ltd","CMP":"2,483","PE":"60.48"},{"name":"Britannia Industries Ltd","CMP":"2,483","PE":"60.48"},{"name":"Titan Company Ltd","CMP":"2,483","PE":"60.48"},{"name":"UPL Ltd","CMP":"2,483","PE":"60.48"},{"name":"Bajaj Finserv Ltd","CMP":"2,483","PE":"60.48"},{"name":"Wipro Ltd","CMP":"2,483","PE":"60.48"},{"name":"ITC Ltd","CMP":"2,483","PE":"60.48"},{"name":"Hero MotoCorp Ltd","CMP":"2,483","PE":"60.48"},{"name":"Ultratech Cement Ltd","CMP":"2,483","PE":"60.48"},{"name":"SBI Life Insurance Company Ltd","CMP":"2,483","PE":"60.48"},{"name":"Bharti Airtel Ltd","CMP":"2,483","PE":"60.48"},{"name":"Oil & Natural Gas Corporation Ltd","CMP":"2,483","PE":"60.48"},{"name":"Coal India Ltd","CMP":"2,483","PE":"60.48"},{"name":"Tata Consumer Products Ltd","CMP":"2,483","PE":"60.48"},{"name":"IndusInd Bank Ltd","CMP":"2,483","PE":"60.48"},{"name":"NTPC Ltd","CMP":"2,483","PE":"60.48"},{"name":"Bajaj Finance Ltd","CMP":"2,483","PE":"60.48"},{"name":"Maruti Suzuki India Ltd","CMP":"2,483","PE":"60.48"},{"name":"Cipla Ltd","CMP":"2,483","PE":"60.48"},{"name":"Eicher Motors Ltd","CMP":"2,483","PE":"60.48"},{"name":"Bajaj Auto Ltd","CMP":"2,483","PE":"60.48"},{"name":"Mahindra & Mahindra Ltd","CMP":"2,483","PE":"60.48"}]
+
+
+for item in stocks:
+    print(item)
+
+ 
